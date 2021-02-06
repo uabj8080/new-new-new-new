22c1b1d032f42a1331e7cd029ad7bfa4c96a0b4a
uabj8000 <uabj8000@gmail.com>
2021-02-05 22:21:36 -0300

    tasting api, cache control, stale while revalidate

--- my-app/pages/api/hello.js
+++ my-app/pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  const date = new Date();
+
+  res.setHeader('Cache-control', 's-maxage=10, stale-while-revalidate');
+
+  res.status(200).json({ 
+    date: date.toUTCString(),
+  });
 }
